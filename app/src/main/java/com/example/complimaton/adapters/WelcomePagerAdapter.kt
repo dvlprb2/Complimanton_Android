c686e12077e7c03f98eaf65eadd0c6ce6ca899b6
developerb2 <bittupatel80@gmail.com>
2023-08-15 09:59:36 -0700

    All completed

--- app/src/main/java/com/example/complimaton/adapters/WelcomePagerAdapter.kt
+++ app/src/main/java/com/example/complimaton/adapters/WelcomePagerAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.complimaton.R
 
@@ -17,28 +18,33 @@ class WelcomePagerAdapter(private val context: Context) :
         R.drawable.getcompliment
     )
 
+    private val text = arrayOf(
+        "Answer crazy polls about friends",
+        "Get Compliments when you picked in a poll",
+        "Find out what your friends are getting picked for"
+    )
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WelcomeViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.item_welcome_page, parent, false)
         return WelcomeViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: WelcomeViewHolder, position: Int) {
-        holder.bind(images[position])
+        holder.bind(images[position], text[position])
     }
 
     override fun getItemCount(): Int = images.size
 
     inner class WelcomeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        fun bind(imageResId: Int) {
+        fun bind(imageResId: Int, imageDesc: String) {
 
             // Bind your image resource to ImageView in your item layout
             // For example:
              val imageView = itemView.findViewById<ImageView>(R.id.imageView)
+             val textView = itemView.findViewById<TextView>(R.id.imageDescription)
              imageView.setImageResource(imageResId)
-
-
-
+            textView.text = imageDesc
         }
     }
 }
